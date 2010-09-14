c270be2aad89a9d9f786a128c7d5855ae7247016
Nishibayashi Takuji <takuji@senchan.jp>
2010-09-15 00:10:49 +0900

    TemplateToolkit使いますよー。

--- lib/Byaalog/Web/Handler.pm
+++ lib/Byaalog/Web/Handler.pm
@@ -5,6 +5,6 @@ use_container 'Byaalog::Container';
 use_context 'Byaalog::Web::Context';
 use_dispatcher 'Byaalog::Web::Dispatcher';
 use_plugins [qw/Encode/];
-use_view 'Kamui::View::MT';
+use_view 'Kamui::View::TT';
 
 1;
