17288fca8b38e41d238865cf884974f3aa42711b
Nishibayashi Takuji <takuji@senchan.jp>
2010-09-17 01:59:14 +0900

    Schema生成スクリプト追加

---
+++ assets/script/make_schema_file.pl
@@ -0,0 +1,10 @@
+package make_schema_file;
+use lib '/home/takuji/project/Byaalog/lib';
+use Kamui;
+use Byaalog::Container;
+use DBIx::Skinny::Schema::Loader qw/make_schema_at/;
+print make_schema_at(
+    'Byaalog::DB::Main::Schema',
+    {},
+    container('conf')->{datasource},
+);
