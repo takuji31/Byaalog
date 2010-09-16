eedbdf9ce30aca682ca142d86b3d2d83261409ed
Nishibayashi Takuji <takuji@senchan.jp>
2010-09-17 03:21:10 +0900

    DBSchema更新

--- lib/Byaalog/DB/Main/Schema.pm
+++ lib/Byaalog/DB/Main/Schema.pm
@@ -1,9 +1,46 @@
 package Byaalog::DB::Main::Schema;
 use DBIx::Skinny::Schema;
+use String::Random;
+use DBIx::Skinny::InflateColumn::DateTime::Auto;
 
 install_table article => schema {
     pk qw/id/;
-    columns qw/id rid title body status created_at updated_at/;
+    columns qw/id rid user_id title body status created_at updated_at/;
+    trigger pre_insert => \&set_rid;
 };
+
+install_table article_category => schema {
+    pk qw/id/;
+    columns qw/id article_id category_id/;
+};
+
+install_table article_comment => schema {
+    pk qw/id/;
+    columns qw/id article_id comment_id/;
+};
+
+install_table category => schema {
+    pk qw/id/;
+    columns qw/id rid name name_en status created_at updated_at/;
+    trigger pre_insert => \&set_rid;
+};
+
+install_table comment => schema {
+    pk qw/id/;
+    columns qw/id rid body status created_at updated_at/;
+    trigger pre_insert => \&set_rid;
+};
+
+install_table user => schema {
+    pk qw/id/;
+    columns qw/id rid login_name password name status rank created_at updated_at/;
+    trigger pre_insert => \&set_rid;
+};
+
+sub set_rid {
+    my ( $class, $args ) = @_;
+    $args->{rid} ||= String::Random->new->randregex('[a-zA-Z0-9]{10}');
+}
+
 
 1;
