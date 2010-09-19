d33e4b8d9521c5162a69471b005c583a96a01492
Nishibayashi Takuji <takuji@senchan.jp>
2010-09-19 12:02:24 +0900

    Validator追加

--- lib/Byaalog/Container.pm
+++ lib/Byaalog/Container.pm
@@ -1,8 +1,15 @@
 package Byaalog::Container;
 use Kamui::Container -base;
 
-register foo => sub {
+register 'validator' => sub {
     my $self = shift;
+    Senchan::Validator->new(
+        {
+            base_class  => 'Byaalog::Validator',
+            message     => $self->get('conf')->{validator_message} || {},
+            constraints => [qw/ Date Email /],
+        }
+    );
 };
 
 1;
