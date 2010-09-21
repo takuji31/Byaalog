f438ae6c1035429fae29e10426c2a0e759caa033
Nishibayashi Takuji <takuji@senchan.jp>
2010-09-21 15:01:34 +0900

    記事編集画面への遷移ルール追加

--- lib/Byaalog/Web/Dispatcher/Admin.pm
+++ lib/Byaalog/Web/Dispatcher/Admin.pm
@@ -9,6 +9,10 @@ on '/user/(.*)' => run {
     return 'Admin::User', $1 || 'index' , FALSE, +{};
 };
 
+on '/article/edit/([a-zA-Z0-9]*)' => run {
+    return 'Admin::Article', 'edit' , FALSE, +{ rid => $1 };
+};
+
 on '/article/(.*)' => run {
     return 'Admin::Article', $1 || 'index' , FALSE, +{};
 };
