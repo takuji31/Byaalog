89cd77ae6619da36a565c4828352e256ae13bcd8
Nishibayashi Takuji <takuji@senchan.jp>
2010-10-24 12:21:10 +0900

    いらないメソッド消し忘れ

--- lib/Byaalog/Model.pm
+++ lib/Byaalog/Model.pm
@@ -1,32 +1,9 @@
 package Byaalog::Model;
-use strict;
-use warnings;
-use Cache::Memory;
-use Byaalog::DB::Main;
+use Potch;
+
 use Byaalog::Container;
 use DBIx::Skinny::ModelLoader setup => {
     skinny => container('db'),
 };
-
-sub get_cache {
-    my $self = shift;
-    my $cache = Cache::Memory->new(namespace => ref $self);
-
-    return $cache;
-}
-
-sub get_cache_expire { 60 }
-
-sub get_db {
-    my $db = Byaalog::DB::Main->new(
-        {
-            dsn => container('conf')->{datasource}->[0],
-            connect_options => { AutoCommit => 1 },
-        }
-    );
-    $db->object_loader('Byaalog::Model');
-    return $db;
-}
-
 1;
 
