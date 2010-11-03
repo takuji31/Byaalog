84be47a036a1e7fc55393004d60c96f2108b3358
Nishibayashi Takuji <takuji@senchan.jp>
2010-11-03 09:11:58 +0900

    methods -> mixin_methods

--- lib/Byaalog/Model.pm
+++ lib/Byaalog/Model.pm
@@ -4,7 +4,7 @@ use Potch;
 use Byaalog::Container;
 use DBIx::Skinny::ModelLoader setup => {
     skinny          => container('db'),
-    methods         => ['search_with_pager'],
+    mixin_methods   => ['search_with_pager'],
     row_class_remap => 1,
 };
 1;
